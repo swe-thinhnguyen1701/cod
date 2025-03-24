4462f633bce4e21e4cb3575c02aae907274c008c
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-03-24 14:56:26 -0500

    BUILD: activateTalent helper method determines if a talent is activate.

---
+++ client/src/services/activateTalent.ts
@@ -0,0 +1,14 @@
+const activateTalent = (group: number, position: number, prerequisite: number[][]): boolean => {
+    // console.log(`[group: ${group},position: ${position}]`);
+    // console.log(prerequisite[group][0]);
+    
+    if(group === 0)
+        return position === 0 ? true : prerequisite[0][position - 1] >= 3;
+    if(group === 1)
+        return prerequisite[0][2] >= 3;
+    if(position === 0 || position === 4)
+        return position === 0 ? prerequisite[1][0] === 1 : prerequisite[group][3] === 1;
+    return prerequisite[group][position - 1] >= 5;
+}
+
+export default activateTalent;
