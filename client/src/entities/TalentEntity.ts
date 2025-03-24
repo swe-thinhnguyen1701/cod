56b78a081e7946c15a2da1f1137b1dfe3fb83619
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-03-24 14:55:40 -0500

    BUILD: generic global Talent entity.

---
+++ client/src/entities/TalentEntity.ts
@@ -0,0 +1,11 @@
+export default interface TalentEnitity {
+    id: number;
+    name: string;
+    description: string[];
+    value: string[];
+    preview: string;
+    level: number;
+    maxLevel: number;
+    group: number;
+    position: number;
+}
