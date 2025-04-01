8d48f33753eb55c0c23bf83658a2347c6fe62ae9
Thinh Nguyen <swe.thinhnguyen@gmail.com>
2025-03-31 23:57:20 -0500

    REFACTOR talent entity to match with backend database schema.

--- client/src/entities/TalentEntity.ts
+++ client/src/entities/TalentEntity.ts
@@ -1,14 +1,22 @@
-export default interface TalentEnitity {
-    id: number;
-    name: string;
-    description: string[];
-    buffValue: string[];
-    debuffValue?: string[];
-    preview: string;
-    level: number;
-    maxLevel: number;
-    group: number;
-    position: number;
-    isPrimaryCore: boolean;
-    isSecondaryCore: boolean;
+export default interface TalentEntity {
+    id: string,
+    name: string,
+    description: string,
+    buff_att: string[] | null,
+    debuff_att: string[] | null,
+    inflict_att: string[] | null,
+    buff_values: string[][] | null,
+    debuff_values: string[][] | null,
+    inflict_values: string[][] | null,
+    is_primary_core: boolean,
+    is_secondary_core: boolean,
+    image: string,
+    max_level: number,
+    prerequisite_talent_core: boolean,
+    group: number,
+    position: number,
+    current_level: number,
+    key: string
 }
+
+// fix the name after done
