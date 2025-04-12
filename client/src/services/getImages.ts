2e0b7568c7360eabd2b41fb9775eb5fd842cd51d
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-04-12 16:39:41 -0500

    BUILD: Create helper method to fetch static images.

---
+++ client/src/services/getImages.ts
@@ -0,0 +1,18 @@
+const GENERAL_HERO_IMAGE_URL =
+  "https://d3bhl6gkk81cq1.cloudfront.net/hero-images/";
+const ZOOM_HERO_IMAGE_URL =
+  "https://d3bhl6gkk81cq1.cloudfront.net/modified-hero-images/";
+
+const heroImageVersion = "v2";
+
+export const getGeneralHeroImage = (heroName: string): string => {
+  return `${GENERAL_HERO_IMAGE_URL}${heroName}-full.webp?${heroImageVersion}`;
+};
+
+export const getHeroAvatar = (heroName: string): string => {
+  return `${GENERAL_HERO_IMAGE_URL}${heroName}-avatar.webp`;
+};
+
+export const getZoomHeroImage = (heroName: string): string => {
+  return `${ZOOM_HERO_IMAGE_URL}${heroName}-full.webp`;
+};
