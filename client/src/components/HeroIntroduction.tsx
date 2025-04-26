7165c6f694da117f68cba45c856e21b496d4963f
Thinh Nguyen <swe.thinhnguyen@gmail.com>
2025-04-26 14:29:51 -0500

    Build and implement HeroIntroduction component on Home page.

---
+++ client/src/components/HeroIntroduction.tsx
@@ -0,0 +1,74 @@
+import { Heading, ListItem, Text, UnorderedList, VStack, Box, Image, HStack, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const HERO_TYPES = ["Infantry", "Cavalry", "Marksman", "Magic", "Overall"];
+const HERO_ROLE_URL = "https://d3bhl6gkk81cq1.cloudfront.net/hero-roles/"
+const ICONIC_HEROES = [
+    {
+        name: "Goresh",
+        title: "Tormentor",
+    }, {
+        name: "Urag",
+        title: "One-Armed Wolf Lord",
+    }, {
+        name: "Maggrat",
+        title: "Seer of Spores",
+    }, {
+        name: "Liliya",
+        title: "Sorceress of Flames",
+    }, {
+        name: "Theia",
+        title: "Unchaning Law",
+    }
+];
+
+const HeroIntroduction = () => {
+    const [selectedType, setSelectedType] = useState(0);
+    return (
+        <>
+            <Flex flexDir={{ base: "column", lg: "row" }} bg="#1b202b" alignItems="center" justifyContent="center" color="white">
+                <VStack gap={8} padding={4} width="100%" maxWidth="700px">
+                    <VStack>
+                        <Text>CHOOSE YOUR</Text>
+                        <Heading as="h2" size="lg" mb={4}>HERO</Heading>
+                        <Text>Whether you thrive on the front lines, strike from the shadows, or rain destruction from afar — there’s a role that fits your style. Command your hero, master your strengths, and shape the battlefield your way.</Text>
+                    </VStack>
+                    <Text className="discover-hero-link" as="span" padding={4}>
+                        <Link to="/heroes" >EXPLORE MORE HEROES</Link>
+                    </Text>
+                    <Box width="100%" overflowX={{ base: "auto", md: "visible" }} minHeight="125px">
+                        <UnorderedList listStyleType="none" display="flex" justifyContent="center" gap={{ base: 4, md: 8 }} minWidth="max-content" margin={0}>
+                            {HERO_TYPES.map((type, index) => (
+                                <ListItem key={index} minWidth="100px" className="hero-role-item" onClick={() => setSelectedType(index)}>
+                                    <VStack>
+                                        <HStack width={{ base: "70px", md: "80px" }}>
+                                            <Image src={`${HERO_ROLE_URL}${type}.webp`} width="100%" alt={`${type} role image`} />
+                                        </HStack>
+                                        <Text
+                                            fontWeight="bold"
+                                            borderBottom={selectedType === index ? "1px solid #f8d375" : "none"}
+                                            color={selectedType === index ? "#f8d375" : "inherit"}
+                                            fontSize={{ base: "10px", md: "12px", lg: "13px" }}
+                                        >
+                                            {type.toUpperCase()}
+                                        </Text>
+                                    </VStack>
+                                </ListItem>
+                            ))}
+                        </UnorderedList>
+                    </Box>
+                </VStack>
+                <VStack paddingBottom={10}>
+                    <Box width={{ base: "100%", md: "500px", lg: "39.06vw" }} maxWidth="800px" height={{ base: "97.19vw", md: "486px", lg: "auto" }}>
+                        <Image src={`https://d3bhl6gkk81cq1.cloudfront.net/hero-images/${ICONIC_HEROES[selectedType].name}-full.webp`} alt={`${ICONIC_HEROES[selectedType].name} hero image`} width="100%" />
+                    </Box>
+                    <Heading as="h4">{ICONIC_HEROES[selectedType].name.toUpperCase()}</Heading>
+                    <Text>{ICONIC_HEROES[selectedType].title}</Text>
+                </VStack>
+            </Flex>
+        </>
+    )
+}
+
+export default HeroIntroduction;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,29 +2,35 @@ import { Box, Image, VStack } from "@chakra-ui/react";
 import logo from "../assets/cod-logo-2.webp"
 import codPhoneBackground from "../assets/cod-phone-background.jpg"
 import DownloadLink from "../components/DownloadLink";
+import HeroIntroduction from "../components/HeroIntroduction";
+import { setPageTitle } from "../services/setTitlePage";
 
 const videoUrl = "https://d3bhl6gkk81cq1.cloudfront.net/videos/intro.webm"
 
 const HomePage = () => {
+    setPageTitle("Call of Dragons Wiki");
     return (
         <Box>
-            <Box maxWidth="1900px" position="relative">
+            <Box position="relative" mb={10}>
                 <Box>
                     <Box display={{ base: "none", md: "block" }}>
-                        <video src={videoUrl} autoPlay muted loop height="200">
+                        <video src={videoUrl} autoPlay muted loop width="100%">
                             Your browser does not support the video.
                         </video>
                     </Box>
-                    <Box display={{base: "block", md: "none"}}>
-                        <Image  src={codPhoneBackground} alt="Call of Dragons background image" />
+                    <Box display={{ base: "block", md: "none" }}>
+                        <Image src={codPhoneBackground} alt="Call of Dragons background image" />
                     </Box>
                 </Box>
                 <VStack position="absolute" zIndex={1} top="50%" left="50%" transform="translate(-50%, -50%)">
-                    <Box display={{base: "none", md: "flex"}} width={{ base: "32.55vw" }}>
+                    <Box display={{ base: "none", md: "flex" }} width={{ base: "32.55vw" }}>
                         <Image src={logo} alt="Call of Dragons logo" />
                     </Box>
                     <DownloadLink />
                 </VStack>
+            </Box>
+            <Box>
+                <HeroIntroduction />
             </Box>
         </Box>
     )
