3b5aa8ffa2950b5f8d54af2b15914fab024a1a44
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-05-01 15:20:02 -0500

    Add heading for Hero and HeroTalent page to improve SEO

--- client/src/pages/Hero.tsx
+++ client/src/pages/Hero.tsx
@@ -1,13 +1,13 @@
 import { useQuery } from "@apollo/client";
 import { GET_ALL_HEROES } from "../graphql/queries";
-import { Heading, HStack, Spinner } from "@chakra-ui/react";
+import { Heading, HStack, Spinner, Text, VStack } from "@chakra-ui/react";
 import HeroCard from "../components/HeroCard"
 import SEO from "../components/SEO";
 import HeroEntity from "../entities/HeroEntity";
 import { Link } from "react-router-dom";
 
 const Hero = () => {
-    const {data, error, loading} = useQuery(GET_ALL_HEROES);
+    const { data, error, loading } = useQuery(GET_ALL_HEROES);
     const heroPageSEO = {
         title: "Heroes - CoD Wiki",
         description: "Explore the heroes in the Call of Dragons Wiki. Discover their roles, skills, and battle stats.",
@@ -15,24 +15,31 @@ const Hero = () => {
         type: "website"
     }
 
-    if(loading)
-        return (<Spinner/>)
+    if (loading)
+        return (<Spinner />)
 
-    if(error)
-        return (<Heading as="h2">Something went wrong</Heading>);
+    if (error)
+        throw new Response("Internal Error", { status: 500 });
 
     const heroes: HeroEntity[] = data.getAllHeroes;
 
     return (
         <>
-        <SEO page={heroPageSEO} />
-        <HStack wrap="wrap" gap={8} margin="0 auto" width="100%" justifyContent="center" className="page">
-            {heroes.map((hero, index) => (
-                <Link to={`/heroes/${hero.name}`} key={index}>
-                    <HeroCard heroName={hero.name} rairtyId={hero.rarity_id} />
-                </Link>
-            ))}
-        </HStack>
+            <SEO page={heroPageSEO} />
+            <VStack pt={8}>
+                <VStack mb={8}>
+                    <Text fontWeight="500">SELECT YOUR</Text>
+                    <Heading as="h1" size="h1">Hero</Heading>
+                    <Text textAlign="center">With more than 40 heroes, you will find a good pair to form a legion</Text>
+                </VStack>
+                <HStack wrap="wrap" gap={8} margin="0 auto" width="100%" maxWidth="1900px" justifyContent="center" className="page">
+                    {heroes.map((hero, index) => (
+                        <Link to={`/heroes/${hero.name}`} key={index}>
+                            <HeroCard heroName={hero.name} rairtyId={hero.rarity_id} />
+                        </Link>
+                    ))}
+                </HStack>
+            </VStack >
         </>
     )
 }
--- client/src/pages/HeroTalent.tsx
+++ client/src/pages/HeroTalent.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, Text, VStack } from "@chakra-ui/react";
 import HeroTalents from "../components/HeroTalents";
 import SEO from "../components/SEO";
 import useTalentStore from "../state-management/talents/store";
@@ -18,10 +18,32 @@ const HeroTalentPage = () => {
     }, []);
     return (
         <>
-            <SEO page={heroTalentPage}/>
-            <Box width="100%" className="page">
-                <HeroTalents />
-            </Box>
+            <SEO page={heroTalentPage} />
+            <VStack>
+                <Box width="100%" maxWidth={"1400px"}>
+                    <VStack mb={8}>
+                        <Heading as="h1" size="h1">Hero Talents</Heading>
+                        <Text>Choose a hero and experiment with different talent builds to find the perfect strategy.</Text>
+                    </VStack>
+                    <Box mb={8}>
+                        <Heading as="h2" mb={4}>HOW TO USE?</Heading>
+                        <VStack alignItems={"flex-start"}>
+                            <Text>
+                                <Text as="span" fontWeight="bold">SELECT A HERO: </Text> Select hero from the list on the left side of the screen.
+                            </Text>
+                            <Text>
+                                <Text as="span" fontWeight="bold">ADD POINT: </Text> Click on the talent icon to add a point to it.
+                            </Text>
+                            <Text>
+                                <Text as="span" fontWeight="bold">REDUCE POINT: </Text> Hold <Text as="span" fontWeight={"bold"}>Alt</Text> (on Window) or <Text as="span" fontWeight={"bold"}>Option</Text> (on Mac) and click on the talent icon to remove a point from it.
+                            </Text>
+                        </VStack>
+                    </Box>
+                    <Box width="100%" className="page">
+                        <HeroTalents />
+                    </Box>
+                </Box>
+            </VStack>
         </>
     )
 }
