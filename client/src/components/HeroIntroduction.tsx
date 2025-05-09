2680a6b1d33c6fe576c98294b9263b910e7df454
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-05-09 14:11:54 -0500

    Add description for each role

--- client/src/components/HeroIntroduction.tsx
+++ client/src/components/HeroIntroduction.tsx
@@ -3,7 +3,27 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { getGeneralHeroImage } from "../services/getImages";
 
-const HERO_TYPES = ["Infantry", "Cavalry", "Marksman", "Magic", "Overall"];
+const HERO_ROLES = [
+    {
+        name: "Infantry",
+        description: "Durable frontline warriors essential for initiating and sustaining engagements. With high defense and health, they anchor battles and can adapt to various situations by using flexible artifacts tailored to the scenario."
+    },
+    {
+        name: "Cavalry",
+        description: "Fast and mobile units with high burst damage, capable of quickly taking down key targets and retreating safely. Excellent for hit-and-run tactics and disrupting enemy formations."
+    },
+    {
+        name: "Marksman",
+        description: "Ranged attackers with high precision, effective at dealing consistent damage from a distance."
+    },
+    {
+        name: "Magic",
+        description: "Spellcasters with powerful area and elemental attacks, often used for crowd control or burst damage."
+    },
+    {
+        name: "Overall",
+        description: "A balanced rating that reflects the hero’s effectiveness across all roles and scenarios."
+    }];
 const HERO_ROLE_URL = "https://d3bhl6gkk81cq1.cloudfront.net/hero-roles/"
 const ICONIC_HEROES = [
     {
@@ -40,11 +60,11 @@ const HeroIntroduction = () => {
                     </Text>
                     <Box width="100%" overflowX={{ base: "auto", md: "visible" }} minHeight="125px">
                         <UnorderedList listStyleType="none" display="flex" justifyContent="center" gap={{ base: 4, md: 8 }} minWidth="max-content" margin={0}>
-                            {HERO_TYPES.map((type, index) => (
+                            {HERO_ROLES.map((type, index) => (
                                 <ListItem key={index} minWidth="100px" className="hero-role-item" onClick={() => setSelectedType(index)}>
                                     <VStack>
                                         <HStack width={{ base: "70px", md: "80px" }}>
-                                            <Image src={`${HERO_ROLE_URL}${type}.webp`} width="100%" alt={`${type} role image`} />
+                                            <Image src={`${HERO_ROLE_URL}${type.name}.webp`} width="100%" alt={`${type.name} role image`} />
                                         </HStack>
                                         <Text
                                             fontWeight="bold"
@@ -52,13 +72,16 @@ const HeroIntroduction = () => {
                                             color={selectedType === index ? "#f8d375" : "inherit"}
                                             fontSize={{ base: "10px", md: "12px", lg: "13px" }}
                                         >
-                                            {type.toUpperCase()}
+                                            {type.name.toUpperCase()}
                                         </Text>
                                     </VStack>
                                 </ListItem>
                             ))}
                         </UnorderedList>
                     </Box>
+                    <Text height={{base: "150px", sm: "120px", md: "75px"}}>
+                        {HERO_ROLES[selectedType].description}
+                    </Text>
                 </VStack>
                 <VStack paddingBottom={10}>
                     <Box width={{ base: "100%", md: "500px", lg: "39.06vw" }} maxWidth="800px" height={{ base: "97.19vw", md: "486px", lg: "auto" }}>
