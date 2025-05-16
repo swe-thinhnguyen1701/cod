57431a94d1a1b19b745ddc61a5b754c0625473cc
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-05-15 23:22:04 -0500

    Refactor fetchData to support new S3 response format.

--- server/seeds/fetchData.js
+++ server/seeds/fetchData.js
@@ -1,14 +1,31 @@
 const s3Client = require("../config/awsConnection");
-const { GetObjectCommand } = require("@aws-sdk/client-s3");
+const { GetObjectCommand, ListObjectVersionsCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const fetch = require("node-fetch");
 require("dotenv").config();
 
-const fetchDataHelper = async (fileName) => {
-    const command = new GetObjectCommand({
+const getLatestVersionId = async (fileName) => {
+    const command = new ListObjectVersionsCommand({
+        Bucket: process.env.BUCKET_NAME,
+        Prefix: fileName
+    });
+
+    const res = await s3Client.send(command);
+    const latestVersion = res.Versions.find(v => v.Key === fileName && v.IsLatest);
+    return latestVersion?.VersionId;
+}
+
+const fetchDataHelper = async (fileName, versionId = null) => {
+    const commandParams = {
         Bucket: process.env.BUCKET_NAME,
         Key: `${fileName}`
-    });
+    };
+
+    if (versionId) {
+        commandParams.VersionId = versionId;
+    }
+
+    const command = new GetObjectCommand(commandParams);
     const url = await getSignedUrl(s3Client, command, { expiresIn: 60 });
     const res = await fetch(url);
     return await res.json();
@@ -16,17 +33,24 @@ const fetchDataHelper = async (fileName) => {
 
 const fetchData = async () => {
     try {
-        const [heroData, artifactData, heroSkillData, heroRoleData, roleCombinationData, roleData, talentCoreData, statData, artifactStatData] = await Promise.all([
-            fetchDataHelper("hero-db.json"),
-            fetchDataHelper("artifact-db.json"),
-            fetchDataHelper("skill-db.json"),
-            fetchDataHelper("hero-role-db.json"),
-            fetchDataHelper("role-combination-db.json"),
-            fetchDataHelper("role-db.json"),
-            fetchDataHelper("talent-core-db.json"),
-            fetchDataHelper("stat-db.json"),
-            fetchDataHelper("artifact-stat-db.json")
-        ]);
+        const FILE_NAMES = [
+            "hero-db.json",
+            "artifact-db.json",
+            "skill-db.json",
+            "hero-role-db.json",
+            "role-combination-db.json",
+            "role-db.json",
+            "talent-core-db.json",
+            "stat-db.json",
+            "artifact-stat-db.json"
+        ];
+
+        const versionIdMap = {};
+        await Promise.all(FILE_NAMES.map(async (fileName) => {
+            versionIdMap[fileName] = await getLatestVersionId(fileName);
+        }))
+
+        const [heroData, artifactData, heroSkillData, heroRoleData, roleCombinationData, roleData, talentCoreData, statData, artifactStatData] = await Promise.all(FILE_NAMES.map(fileName => fetchDataHelper(fileName, versionIdMap[fileName])));
 
         return { heroData, artifactData, heroSkillData, heroRoleData, roleCombinationData, roleData, talentCoreData, statData, artifactStatData };
     } catch (error) {
